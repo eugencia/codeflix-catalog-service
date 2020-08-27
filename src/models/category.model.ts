bd950dd3fc4aadd4a4f45c6e5280c26b959c4fbe
Eduardo Cavalcante <epscavalcant@gmail.com>
2020-08-27 19:38:49 -0400

    Definindo propriedades de categoria

--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -17,15 +17,23 @@ export class Category extends Entity {
   name: string;
 
   @property({
-    type: 'string',
+    type: 'boolean',
+    required: false,
+    default: true
   })
-  description?: string;
+  is_active: boolean;
 
-  // Define well-known properties here
+  @property({
+    type: 'date',
+    required: true
+  })
+  created_at: Date
 
-  // Indexer property to allow additional data
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [prop: string]: any;
+  @property({
+    type: 'date',
+    required: true
+  })
+  updated_at: Date
 
   constructor(data?: Partial<Category>) {
     super(data);
